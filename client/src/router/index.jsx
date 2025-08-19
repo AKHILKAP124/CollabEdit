a1e9ee47153b99797022caba14c2963858852810
Akhil Kumar <75962924+AKHILKAP124@users.noreply.github.com>
2025-08-19 19:47:11 +0530

    fix bug

--- client/src/router/index.jsx
+++ client/src/router/index.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Editor from "../app/Editor/Page";
-import SnippetsPage from "../app/snippet/page";
+import SnippetsPage from "../app/snippet/Page";
 import Snippet from "../app/snippet/snippetPage/Page";
 
 const AppRouter = createBrowserRouter([
